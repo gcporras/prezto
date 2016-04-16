b3cda72b7419aaf92e169665a542cd0d9bb09b56
gporras <g.porras@onf.ca>
2016-04-16 10:53:30 -0400

    Backup from work mac

--- modules/prompt/init.zsh
+++ modules/prompt/init.zsh
@@ -5,6 +5,26 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
+# PATCH: Should be moved to a better place maybe
+# function to get the short version of the current path
+# requires PWD_LENGTH variable to be set
+# defaults to 30, if not set
+function get_short_path() {
+  HOME_TIDLE="\/Users\/guy"
+    LONG_PATH=`pwd | sed -e "s/$HOME_TIDLE/\~/"`
+
+    # check to see if the prompt path length has been specified
+    if [ ! -n "$PWD_LENGTH" ]; then
+        export PWD_LENGTH=15
+    fi
+
+    if [ ${#LONG_PATH} -gt $PWD_LENGTH ]; then
+            echo "...${LONG_PATH: -$PWD_LENGTH}"
+    else
+            echo $LONG_PATH
+    fi
+}
+
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
